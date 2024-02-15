47b7632a83eaffdff4ac78666d633f67259ba217
Silca93 <eric_delhaye@.com>
2024-02-15 10:08:44 +0100

    first commit with the header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="container w-1/2 h-1/5 flex justify-center">
+        <div className="w-full h-full border-2 border-solid border-white rounded-xl mt-10 leading-none  pl-5 pr-5 flex justify-between">
+          <div className="left w-1/2 h-full flex flex-col items-start justify-center">
+            <h1 className="text-white text-[2.25rem] ">ROCK</h1>
+            <h1 className="text-white text-[2.25rem] ">PAPER</h1>
+            <h1 className="text-white text-[2.25rem] ">SISSORS</h1>
+          </div> 
+          <div className="right w-1/2 h-full flex flex-col items-end justify-center">
+            <div className="square w-[140px] h-[110px] bg-white rounded-md flex flex-col gap-4 items-center justify-center">
+                <h1>SCORE</h1>
+                <h1 className="text-[3.15rem]">0</h1>
+            </div>
+          </div>   
+        </div>
+    </div>
+  )
+}
